44d71028190d5e5efdd7fa5355b290dbd85bbe79
fgarciabarrios <fgarciabarrios@gmail.com>
2024-09-26 12:56:04 -0500

    Creación de app en Netbeans

---
+++ planificador/src/main/java/com/usb/planificador/Planificador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.usb.planificador;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Planificador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
